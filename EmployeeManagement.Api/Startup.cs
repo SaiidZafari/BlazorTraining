2c2fbee1e4efec51b1ee4e673b4de5a64041b036
SaiidZafari <saiid.zafari@bahnhof.se>
2021-05-15 14:06:51 +0200

    Add Database Connection to API

--- EmployeeManagement.Api/Startup.cs
+++ EmployeeManagement.Api/Startup.cs
@@ -1,7 +1,10 @@
+using EmployeeManagement.Api.Models;
+using EmployeeManagement.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,7 @@ namespace EmployeeManagement.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DBConnection")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
