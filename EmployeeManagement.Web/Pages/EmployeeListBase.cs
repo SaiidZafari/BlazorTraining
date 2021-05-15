b7efdf267f9892cb2a274d654fc4d5db6ccca292
SaiidZafari <saiid.zafari@bahnhof.se>
2021-05-15 12:16:35 +0200

    Add EmployeeManagement.Api to make a REST API

--- EmployeeManagement.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeListBase.cs
@@ -18,7 +18,7 @@ namespace EmployeeManagement.Web.Pages
 
         private void LoadEmployees()
         {
-            System.Threading.Thread.Sleep(3000);
+            //System.Threading.Thread.Sleep(3000);
             Employee e1 = new Employee
             {
                 EmployeeId = 1,
@@ -27,7 +27,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "David@pragimtech.com",
                 DateOfBrith = new DateTime(1980, 10, 5),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 1,
                 PhotoPath = "images/john.png"
             };
 
@@ -39,7 +39,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "Kim@pragimtech.com",
                 DateOfBrith = new DateTime(1981, 12, 22),
                 Gender = Gender.Male,
-                Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
+                DepartmentId = 2,
                 PhotoPath = "images/Kim.png"
             };
 
@@ -51,7 +51,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "mary@pragimtech.com",
                 DateOfBrith = new DateTime(1979, 11, 11),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
+                DepartmentId = 3,
                 PhotoPath = "images/mary.png"
             };
 
@@ -63,7 +63,7 @@ namespace EmployeeManagement.Web.Pages
                 Email = "sara@pragimtech.com",
                 DateOfBrith = new DateTime(1982, 9, 23),
                 Gender = Gender.Female,
-                Department = new Department { DepartmentId = 3, DepartmentName = "Payroll" },
+                DepartmentId = 4,
                 PhotoPath = "images/sara.png"
             };
 
