0188c29ce93aa728c1cb823de5c00e4b64a24ea5
SaiidZafari <saiid.zafari@bahnhof.se>
2021-05-16 10:50:08 +0200

    Add Coordinat and Hide Footer to Web

--- EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagement.Web/Pages/EmployeeDetailsBase.cs
@@ -1,6 +1,7 @@
 ﻿using EmployeeManagement.Models;
 using EmployeeManagement.Web.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,11 @@ namespace EmployeeManagement.Web.Pages
 
         public Employee Employee { get; set; } = new Employee();
 
+        protected string Coordinates { get; set; }
+
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
+
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
@@ -23,5 +29,26 @@ namespace EmployeeManagement.Web.Pages
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
         }
+
+        // Button Hide footer
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "ShowFooter";
+                CssClass = "HideFooter";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
+        }
+
+        //// Cordinat Alternavie solution
+        //protected void Mouse_Move(MouseEventArgs e)
+        //{
+        //    Coordinates = $"X ={e.ClientX} Y = {e.ClientY}";
+        //}
     }
 }
